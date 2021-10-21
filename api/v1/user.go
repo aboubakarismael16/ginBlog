f74d5fb4b9ebd1b8f8e3e03558252437ec93cc0d
aboubakar ismael <aboubakarismael16@gmail.com>
2021-10-21 21:13:38 +0800

    add update

--- api/v1/user.go
+++ api/v1/user.go
@@ -5,14 +5,16 @@ import (
 	"ginBlog/utils/errmsg"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
+var code int
 
 func AddUser(c *gin.Context)  {
-	// todo
+
 	var data model.User
 	_  = c.ShouldBindJSON(&data)
-	code := model.CheckUser(data.Username)
+	code = model.CheckUser(data.Username)
 	if code == errmsg.SUCCSE {
 		model.CreateUser(&data)
 	}
@@ -28,7 +30,23 @@ func AddUser(c *gin.Context)  {
 }
 
 func GetUser(c *gin.Context)  {
-	
+	paseSize, _:= strconv.Atoi(c.Query("pagesize"))
+	pageNum,_ := strconv.Atoi(c.Query("pagenum"))
+
+	if paseSize == 0 {
+		paseSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = -1
+	}
+
+	data := model.GetUsers(paseSize,pageNum)
+	code = errmsg.SUCCSE
+	c.JSON(http.StatusOK, gin.H{
+		"status":code,
+		"data": data,
+		"message":errmsg.GetErrMsg(code),
+	})
 }
 
 func EditUser(c *gin.Context)  {
--- model/User.go
+++ model/User.go
@@ -7,9 +7,9 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"type:varchar(20);not null " json:"username" validate:"required,min=4,max=12" label:"用户名"`
-	Password string `gorm:"type:varchar(500);not null" json:"password" validate:"required,min=6,max=120" label:"密码"`
-	Role     int    `gorm:"type:int;DEFAULT:2" json:"role" validate:"required,gte=2" label:"角色码"`
+	Username string `gorm:"type:varchar(20);not null " json:"username"`
+	Password string `gorm:"type:varchar(20);not null" json:"password"`
+	Role     int    `gorm:"type:int" json:"role"`
 }
 
 func CheckUser(name string) (code int)  {
@@ -22,6 +22,19 @@ func CheckUser(name string) (code int)  {
 	return errmsg.SUCCSE //200
 }
 
+// CheckUpUser 更新查询
+func CheckUpUser(id int, name string) (code int) {
+	var user User
+	db.Select("id, username").Where("username = ?", name).First(&user)
+	if user.ID == uint(id) {
+		return errmsg.SUCCSE
+	}
+	if user.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED //1001
+	}
+	return errmsg.SUCCSE
+}
+
 func CreateUser(data *User) int  {
 	err := db.Create(&data).Error
 	if err != nil {
@@ -30,3 +43,15 @@ func CreateUser(data *User) int  {
 	
 	return errmsg.SUCCSE // 200
 }
+
+
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err = db.Limit(pageSize).Offset((pageNum-1)*pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+
+	return users
+}
+
